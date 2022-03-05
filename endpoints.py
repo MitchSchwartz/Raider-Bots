5e3608c3692aa211e3a5e8bc933141d5b5cb64bf
Mitch Schwartz <mitch@pragmaflow.com>
2022-03-05 09:06:56 -0500

    nomics api fix

--- endpoints.py
+++ endpoints.py
@@ -9,7 +9,7 @@ from get_token_values import getTokenValues
 from tournament_timer_update import tournamentTimerUpdate
 from reset_timer_update import resetTimerUpdate
 
-
+i = 0
 
 
 app = Flask('')
@@ -18,7 +18,7 @@ app = Flask('')
 
 def updateAlltheBots():
   start = datetime.now()
-  i = 0
+ 
   
   print("\n", f">>> RUNNING updateAlltheBots  - {start} \n")
   
