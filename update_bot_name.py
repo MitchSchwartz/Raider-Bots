2aa3128f254ee55a37f046e1b21244359c00c47c
Mitch <72631198+Mitch-PragmaFlow@users.noreply.github.com>
2022-03-18 13:27:20 +0000

    updates for liveonly and tesmode

--- update_bot_name.py
+++ update_bot_name.py
@@ -1,20 +1,36 @@
 import os
 import requests
-
+from test_mode import testMode, liveOnly
 from get_server_id import getServerId
 #from json import dumps
 
 
 def botNameUpdate(_newName, _tokenName, _server):
 
+
+  if(liveOnly and _server == "test"):
+    print("Skipping due to liveOnly")
+    return
+
+  if(testMode and _server != "test"):
+    print("Skipping due to testMode")
+    return
+  
+    
+  
   server = getServerId(_server)
 
+  
+    
+
+  
+  
   auth = "Bot " + os.environ.get(str(_tokenName))
   url = f'https://discordapp.com/api/guilds/{server}/members/@me'        
   headers = {'Authorization': auth, 'Content-Type': 'application/json'}
   payload = {'nick': _newName}
 
-
+  
   try:  
     r = requests.patch(url, json=payload, headers=headers)
     #r.json()
