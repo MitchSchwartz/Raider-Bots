ea6ef8b354e9e0562cf9d3fe1f16a7fbae26ea5a
Mitch Schwartz <mitch@pragmaflow.com>
2022-03-19 07:19:00 -0400

    cleanup

--- endpoints.py
+++ endpoints.py
@@ -1,34 +1,12 @@
 from flask import Flask
 from threading import Thread
-from datetime import datetime
-from time import sleep as sleep
-import requests
-
-
-from get_token_values import getTokenValues
-from tournament_timer_update import tournamentTimerUpdate
-from reset_timer_update import resetTimerUpdate
-
-
-
 
 app = Flask('')
-
-
-
-
-
 
 @app.route('/')
 # pinged every minute by https://console.cron-job.org/dashboard
 def ping(_fakeParam1, _fakeParam2):
-  return('ok')
-      
-    
-        
-
-
- 
+  return('ok') 
 
 
 def run():
