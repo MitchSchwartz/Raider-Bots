11943dc04458270fa552b4e22494ae3152ab8c28
Mitch-PF <mitch@pragmaflow.com>
2022-06-23 03:58:23 -0400

    tournament bot cooldown period

--- scripts/endpoints.py
+++ scripts/endpoints.py
@@ -6,7 +6,7 @@ import requests
 import os
 
 from scripts.get_token_values import getTokenValues
-from scripts.tournament_timer_update import tournamentTimerUpdate
+import scripts.tournament_timer_update as tt
 from scripts.reset_timer_update import resetTimerUpdate
 
 testMode = os.getenv("testMode")
@@ -15,70 +15,77 @@ testMode = os.getenv("testMode")
 app = Flask('')
 
 
-
 def updateAlltheBots():
-  start = datetime.now()
-  
-  print("\n", f">>> RUNNING updateAlltheBots  - {start} \n")
-  
-  while True:
-    
-    
-    try:
-      outcomeTokenValues = getTokenValues()
-    except Exception as e:
-      print(f"!!!Token Bot {e} Update error  \n!!!{outcomeTokenValues}")
-    
-    try:    
-      outcomeResetTimer = resetTimerUpdate()
-    except Exception as e:
-      print(f"!! resetTimer Bot Update error:\n!!{e} \n!!{outcomeResetTimer}")
-    
+    start = datetime.now()
+    print("\n", f">>> RUNNING updateAlltheBots  - {start} \n")
 
-    try:
-      tournamentTimerUpdate("cr")
-    except Exception as e:
-      print(f"!! tournament bot error:\n {e}")
-    
-    if testMode =="True":
-      print("\n skipping watchdog due to testMode")
-    else:
-      url = 'https://nosnch.in/74bee12403'
-      headers = {'Content-Type': 'application/x-www-url-formencoded'}
-      r = requests.post(url, headers=headers)
-      print(r)
-      
-    end = datetime.now()
-    print(f"\n >>> updateAlltheBots Executed - {end} \n")
-    
-    sleepTime = end - start
-    print(f"sleep time: {sleepTime}")
-    sleep(50)
+    tt.skipDueToRateLimit = False  # for tourney bot 429 handling
 
-  
+    while True:  # forever
 
-    
+        ### Token Bots ###
+        try:
+            outcomeTokenValues = getTokenValues()
+        except Exception as e:
+            print(f"!!!Token Bot {e} Update error  \n!!!{outcomeTokenValues}")
 
+        ### Reset Timer Bot ###
+        try:
+            outcomeResetTimer = resetTimerUpdate()
+        except Exception as e:
+            skipDueToRateLimit = True
+            print(
+                f"!! resetTimer Bot Update error:\n!!{e} \n!!{outcomeResetTimer}")
+
+        ### Tournament Timer Bot Update ###
+        if (not tt.skipDueToRateLimit or tt.skipCount > tt.skipAmount):
+
+            try:
+                tt.tournamentTimerUpdate("cr")
+
+            except Exception as e:
+
+                if(tt.skipCount > 0):
+                    tt.skipDueToRateLimit = True
+                    print(
+                        f"Tournament Bot Error, retrying once before skipping {tt.skipAmount} times")
+
+                tt.skipCount += 1
+                print(f"!! tournament bot error:\n {e}")
+
+            else:
+                tt.skipCount = 0
+                tt.skipDueToRateLimit = False
+        else:
+            tt.skipCount += 1
+            print(
+                f"\n>>> Skipping Tournament Bot because: recent error. skipCount is {tt.skipCount} / {tt.skipAmount}")
+
+        if testMode == "True":
+            print("\n skipping watchdog due to testMode")
+        else:
+            url = 'https://nosnch.in/74bee12403'
+            headers = {'Content-Type': 'application/x-www-url-formencoded'}
+            r = requests.post(url, headers=headers)
+            print(f"Watchdog says: {r}")
+
+        end = datetime.now()
+        print(f"\n >>> updateAlltheBots Executed - {end} \n")
+
+        sleepTime = end - start
+        print(f"sleep time: {sleepTime}")
+        sleep(50)
 
 
 @app.route('/')
-
 def ping():
-  return('ok')
-      
-    
-        
-
-
- 
+    return('ok')
 
 
 def run():
-    app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 
 def startFlask():
     t = Thread(target=run)
     t.start()
-
-    
--- scripts/tournament_timer_update.py
+++ scripts/tournament_timer_update.py
@@ -1,9 +1,8 @@
 import os
 import pytz
 import requests
-from scripts.bot_class_def import botList 
+from scripts.bot_class_def import botList
 from scripts.date_functions import findTimeDiff, makeTimerStr
-
 from datetime import datetime
 from dateparser import parse as dParse
 #from json import dumps
@@ -11,105 +10,94 @@ from dateparser import parse as dParse
 #from operator import itemgetter
 
 
-
-
+skipDueToRateLimit = False
+skipCount = 0
+skipAmount = 20
 
 
 def getEvents(_server):
-  auth = "Bot " + os.environ.get(str("resetTimerBot"))
+    auth = "Bot " + os.environ.get(str("resetTimerBot"))
 
-  guildId = 860057024611876865
+    guildId = 860057024611876865
 
-  url = f"https://discordapp.com/api/v9/guilds/{guildId}/scheduled-events"
-  headers = {'Authorization': auth, 'Content-Type': 'application/json'}
-  
-  try:
-    r = requests.get(url, headers=headers)
-  except requests.exceptions.RequestException as e:
-    print(f"\n {e} \n")# {r.content} \n")
-    
-    
-  response = r.json()
-  #print (dumps(response, indent=4))
-  return response
+    url = f"https://discordapp.com/api/v9/guilds/{guildId}/scheduled-events"
+    headers = {'Authorization': auth, 'Content-Type': 'application/json'}
 
+    try:
+        r = requests.get(url, headers=headers)
+    except requests.exceptions.RequestException as e:
+        # {r.content} \n")
+        print(f"\nError getting events from Discord: {e} \n")
+
+    response = r.json()
+    #print (dumps(response, indent=4))
+    if (r == 429):
+        skipDueToRateLimit = True
+    return response
 
 
 def getNextEventStart(e):
 
-  try:
-    #e.sort(key=itemgetter('choice'), reverse=False)
-    #print("e:", e)
-    e.sort(key = lambda x:x["scheduled_start_time"])
+    try:
+        #e.sort(key=itemgetter('choice'), reverse=False)
+        #print("e:", e)
+        e.sort(key=lambda x: x["scheduled_start_time"])
 
-    nextStart = dParse(e[0]['scheduled_start_time'])
-    #print(f"\n >>>next start {nextStart}")#" \n >>sorted: {dumps(response, indent =4)}\n")
+        nextStart = dParse(e[0]['scheduled_start_time'])
+        # print(f"\n >>>next start {nextStart}")#" \n >>sorted: {dumps(response, indent =4)}\n")
 
-    return nextStart
+        return nextStart
 
-  except requests.exceptions.RequestException as e:
-    print(f"\n {e} \n {e.content} \n")
-    return ("! We had an problem getting events from discord")
-    
+    except requests.exceptions.RequestException as e:
+        print(
+            f"\n ! We had an problem getting events from discord: {e} \n {e.content} \n")
+        return
 
 
 def tourneyTimeDiff(nextStart):
-  
-  UTC = pytz.timezone('UTC')
-  now = datetime.now(UTC)
-  
-  
-  #print(f"\n>>>nextStart: {nextStart}\n")
-  tourneyTimeDiff, past = findTimeDiff(now, nextStart)
-  #print(f"\n>>>tourneyTimediff: {tourneyTimeDiff}\n")
 
-  tourneyTimeDiff = makeTimerStr(tourneyTimeDiff)
-  #print(f"\n>>>tourneyTimediff2: {tourneyTimeDiff}\n")
+    UTC = pytz.timezone('UTC')
+    now = datetime.now(UTC)
 
-  if past:
-    tourneyTimeDiff = f"- {tourneyTimeDiff}"
+    #print(f"\n>>>nextStart: {nextStart}\n")
+    tourneyTimeDiff, past = findTimeDiff(now, nextStart)
+    #print(f"\n>>>tourneyTimediff: {tourneyTimeDiff}\n")
 
-  #print(r.json(), r.content)
-  return tourneyTimeDiff
+    tourneyTimeDiff = makeTimerStr(tourneyTimeDiff)
+    #print(f"\n>>>tourneyTimediff2: {tourneyTimeDiff}\n")
 
-  
-  
+    if past:
+        tourneyTimeDiff = f"- {tourneyTimeDiff}"
+
+    #print(r.json(), r.content)
+    return tourneyTimeDiff
+
+
 def tournamentTimerUpdate(_server):
-  
 
-  events = False
-  events = getEvents(_server)
-  newBotName = "Event: TBA"
-  #print(f">>>Events:{events}")
+    # cool down period if rate limit is hit
+    if (skipDueToRateLimit == True and skipCount < skipAmount):
+        skipCount += 1
+        return (f"\n>>> Skipped due to rate limit, Skipcount is now {skipCount}")
 
-  
-  if (not events):
-    return
+    events = False
+    events = getEvents(_server)
+    newBotName = "Event: TBA"
+    # print(f">>>Events:{events}")
 
-  try:
-    nextStart =  getNextEventStart(events)
-    print("\n>>>there's an event!")
-    newBotName = f"Event: {tourneyTimeDiff(nextStart)}"
-  except requests.exceptions.RequestException as e:
-    print(f"\n>>>Error: {e}")# "\n", dumps(r.content), "\n")
-    
-    
-  try:
-    botList["tourneyBot"].updateBot({newBotName})
-    print("tourney bot name updated")
-  except requests.exceptions.RequestException as e:
-    print(f"\n {e} \n")
-    
-  
+    if (not events or len(events) == 0):
+        return (f"\n>>> Skipped due to: no events returned")
 
-    
+    try:
+        nextStart = getNextEventStart(events)
+        print("\n>>>there's an event!")
+        newBotName = f"Event: {tourneyTimeDiff(nextStart)}"
+    except requests.exceptions.RequestException as e:
+        # "\n", dumps(r.content), "\n")
+        print(f"\n>>>Error getting next event: {e}")
 
-
-
-
-
-
-
-
-
-
+    try:
+        botList["tourneyBot"].updateBot({newBotName})
+        print("tourney bot name updated")
+    except requests.exceptions.RequestException as e:
+        print(f"\n Tournament Bot Name Update Error: {e} \n")
