7ac1b7fed4db553dd80c2263a4040a402d58b6b2
Mitch-PF <mitch@pragmaflow.com>
2022-06-28 17:08:46 -0400

    partially working

--- scripts/date_functions.py
+++ scripts/date_functions.py
@@ -2,7 +2,7 @@ import datetime
 
 
 def findTimeDiff(_time1, _time2):
-    print(f"\n>>>time1: {_time1} | time2: {_time2}")
+    #print(f"\n>>>time1: {_time1} | time2: {_time2}")
     timeDiff = _time2 - _time1
     past = False
     if (timeDiff < datetime.timedelta(0)):
